47b78714cb4c893fee4c679d6f141c71720cbac8
Uttam Boda <up6440631@gmail.com>
2026-02-26 16:22:22 +0530

    change base url

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = 'https://fine-very-photographs-down.trycloudflare.com';
 
 // ==========================================
 // API SERVICE HANDLER & UTILS
